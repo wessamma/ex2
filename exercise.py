975a576d3d250d653d8c0d9395ab132c52a376d3
wessamma <engbomokdobwe@gmail.com>
2022-08-10 09:29:45 +0200

    hi

---
+++ exercise.py
@@ -0,0 +1,25 @@
+# do exercise here
+# upload to github for portfolio
+# thank me later when your git graph is as green as the python money u gonna earn
+
+#EX2
+
+#Task 1
+
+a = 34
+b = 55
+c = 54.5
+d = -5
+
+print('a+b = ',a+b)
+print('a-b = ',a-b)
+print('abs(a-b) = ',abs(a-b))
+print('a*b = ',a*b)
+print('c/b = ',c/b)
+print('d//b = ',d//b)
+print('a**b = ',a**b)
+print('round(a//b) = ',round(a//b,5))
+print('a%b = ',a%b)
+
+
+#Task 2
