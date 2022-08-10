09b71f3078dbf0a647bdce25786ac13a6016bbb4
wessamma <engbomokdob@gmail.com>
2022-08-10 15:03:14 +0200

    ex2

--- exercise.py
+++ exercise.py
@@ -6,20 +6,46 @@
 
 #Task 1
 
+print('````bach')
+
 a = 34
 b = 55
 c = 54.5
 d = -5
+e = 5+2j
+f = 0
 
-print('a+b = ',a+b)
-print('a-b = ',a-b)
-print('abs(a-b) = ',abs(a-b))
-print('a*b = ',a*b)
-print('c/b = ',c/b)
-print('d//b = ',d//b)
-print('a**b = ',a**b)
-print('round(a//b) = ',round(a//b,5))
-print('a%b = ',a%b)
+print ( a ,'+',b ,'=',a+b )
+print(a,'-',b, '=',a-b)
+
+print(a,'*',b ,'=',a*b)
+print(c,'/',b, '=',c/b)
+
+# print(c,'/',f, '=',c/f) division by zero .
+
+print(d,'//',b ,'=',d//b)
+print(a,'**',b ,'=',a**b)
+
+print(a,'%',b, '= ',a%b)
+
+print('````')
 
 
 #Task 2
+
+print('````bach')
+
+print('max(',a,',',c,',',b,') =',max(a,c,b))
+print('min(',a,',',c,',',b,') =',min(a,c,b))
+print('abs(',d,')= ',abs(d))
+print('power(',b,',',a,') =',  pow(a,b))
+
+import math
+
+print('ceil(',c,'/',d,') =', math.ceil(c/d))
+print('floor(',c,'/',d,') =', math.floor(c/d))
+print('abs(',e,') =',abs(e))
+
+
+
+print('````')
